723147ce5afc686a715db6a4441ba2e77e551b19
Skylar Ittner <admin@netsyms.com>
2016-08-24 12:11:59 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+To run
+-------------
+From this folder, type:
+
+    java -jar dist/HelloWorld.jar
+
+or:
+
+    cd src; java name/skylarismy/HelloWorld
---
+++ src/name/skylarismy/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name.skylarismy;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author skylar
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world");
+    }
+
+}
